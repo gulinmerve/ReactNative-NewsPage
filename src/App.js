7c036168cc78b5a2858e57dd4058192fe8e61163
Merve <gulineyel@gmail.com>
2020-10-08 18:27:34 +0300

    project started

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+
+const App = () =>  {
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
